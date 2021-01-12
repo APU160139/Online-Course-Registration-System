33758deb5c6185f9342028eafcb22d53485fb54b
MD APU HOSEN <47341461+APU160139@users.noreply.github.com>
2021-01-12 11:41:11 +0600

    Add files via upload

---
+++ Hall Fee.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="css/registration.css">
+    <link rel="stylesheet" href="css/style.css">
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<style>
+.col-75 {
+  padding: 0 16px;
+}
+
+</style>
+
+
+</head>
+
+<div class="header">
+<img src="Image/web.jpg" alt="Trulli" width="900" height="150">    
+</div>
+
+<body>
+  
+
+<div class="content">
+      <div class="topnav">
+            <a href="Home page.php">Instruction </a>
+            <a href="Registration.php">Registration</a>
+            <a class="active" href="#Payment">Payment</a>
+            <a href="login.php">Login</a>
+          </div>
+          
+          <div style="height: 400px;
+          width: 900px;
+          padding-top: 50px;
+          background-color: powderblue">
+          
+          <div>
+          <div class="rows">
+            <div class="col-75">
+              <div class="container">
+                <form style="height: 240px;
+                width: 835px;
+                padding-top: 0px;
+                background-color: rgb(247, 215, 215);border:15px" >
+                
+                      <h3 style="background-color: rgb(201, 198, 199); text-align: center"> Hall Fee</h3>
+                      
+                      <label for="cname">Application id</label>
+                      <br>
+                      <input type="text" id="cname" name="cardname">
+                      <br>
+                      <label for="cname">Bank Recipt Number</label>
+                      <br>
+                      <input type="text" id="cname" name="cardname">
+                      <br>
+                      <label for="ccnum">Bank Recipt Photos</label>
+                      <input type="file" id="ccnum" name="cardnumber">
+                      <div >
+                      <input type="submit" value="submit"style=" background-color: #4CAF50;
+                      color: white;
+                      padding: 12px;
+                      margin: 10px 0;
+                      border: none;
+                      width: 92.8%;
+                      border-radius: 3px;
+                      cursor: pointer;
+                      font-size: 17px;"> 
+                      </div> 
+
+                      
+                      </form>   
+                     
+                          </div>
+                        </div>  
+                      </div> 
+                    </div>
+                 </div>
+                 <div style="height: 50px;
+          width: 900px;
+          background-color: rgb(43, 75, 75);
+          color: white;
+         
+          text-align: center;
+          padding-top: 20px;"
+          
+            <p> © 2019 Department of Computer Science and Engineering, Jashore University of Science and Technology</p>
+          </div>   
+            </body>
+</html>
---
+++ Home page.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="css/registration.css">
+<link rel="stylesheet" href="css/style.css">
+</head>
+    <div class="header">
+    <img src="Image/web.jpg" alt="Trulli" width="900" height="150">
+    
+</div>
+
+<body>
+<div class="content">
+      <div class="topnav">
+            <a class="active" href="">Instruction </a>
+            <a href="Registration.php">Registration</a>
+            <a href="PAY.php">Payment</a>
+            <a href="login.php">Login</a>
+          </div>
+          
+          <div style="height: 650px;
+          width: 900px;
+          padding-top: 10px;
+          padding-left:15px;
+          padding-right:15px;
+          background-color: powderblue">
+            
+             <h4><strong>Registration: </strong></h4>
+             <p> All the new students must register their courses in the prescribed Registration Form at the time of admission.
+
+                Existing students must register their courses of the new semester before the classes of the new semester start on payment of their semester tuition and other fees or their parts as determined by the university administration. Students should consult the Faculty in Charge/Chairman of the Department/Program Director (in case of MBA, EMBA & MBM Programs) in planning their course schedule and should follow the rules and procedures related to registration for courses.</p>
+                <h4><strong>Late Registration: </strong></h4>
+                <p>A student who seeks to register after the first day of classes must obtain permission from the office of the university. Those students who are given permission to register late may have to pay a late registration fee.</p>
+                <h4><strong>Retake Course Registration: </strong></h4>
+                <p>Students scoring 'F' grade in any course shall have to pay full tuition fees for repeating the course. Students with grade lower than "A" in any course may retake the course to improve their grades and shall have to pay half of the tuition fees for the course/courses.</p>
+                <h4><strong>Re-admisson Registration: </strong></h4>
+                <p>Students may repeat courses in order to improve their grades. They must register for and complete the course/courses to accomplish the repeat and 50% of the tuition fees has to be paid for the course/courses repeated. However, a student who receives "F" grade in any course must repeat that course and obtain a minimum of "D" grade in order to meet degree requirements. Both the original and the repeat grades are recorded in the transcript, but CGPA is calculated on the basis of the better of the two grades. A course can be repeated once only.</p>
+                <h4><strong>Research: </strong></h4>
+                <p>Students have to undertake a semester-long research under the supervision of a faculty and earn a minimum of C grade for graduation where such research is a requirement for the degree.</p>
+                <h4><strong>Internship Requirement: </strong></h4>
+                <p>Students registering for internship should preferably have completed all course work. Students may not be registered for any course during internship. For purposes of fulfilling degree requirements, students should be interned in organizations ideally for twelve weeks, allowing two to four weeks for writing reports, etc. The students must earn at least a C grade in the internship project.</p>
+         
+            
+          </div>
+
+          <div style="height: 50px;
+          width: 900px;
+          background-color: rgb(43, 75, 75);
+          color: white;
+         
+          text-align: center;
+          padding-top: 20px;"
+          
+            <p> © 2019 Department of Computer Science and Engineering, Jashore University of Science and Technology</p>
+          </div>
+
+  
+
+</body>
+</html>
